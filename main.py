513485175625d5bf5d4e24122989846aae6207cd
Julian Dale <julian.h.dale@gmail.com>
2024-07-21 21:37:33 -0500

    adding the data report endpoint

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ import hug
 from pymongo import MongoClient
 from datetime import datetime
 import json
+from typing import Any
+from bson import ObjectId
+
 
 db_client = MongoClient('localhost', 27017)
 test_collection = db_client.test_db.test_collection
 weather_collection = db_client.weather.barometric
+
+
+class MongoJSONEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime):
+            return str(o)
+        return super().default(o)
+
 
 @hug.get("/datapoint")
 def save_data_point(pressure:hug.types.number, temp:hug.types.number, humidity:hug.types.number):
@@ -18,12 +31,24 @@ def save_data_point(pressure:hug.types.number, temp:hug.types.number, humidity:h
     }
     print(datapoint)
     id = weather_collection.insert_one(datapoint)
-    return {
-        "result": "200 ok"
-    }
+    data_json = MongoJSONEncoder().encode(datapoint)
+    return json.loads(data_json)
+
+@hug.get("/datareport")
+def get_reported_data(date):
+    curs = weather_collection.find({"date": date})
+    data_json = MongoJSONEncoder().encode(list(curs))
+    return json.loads(data_json)
+
+
+@hug.get("/happy_birthdayreport")
+def get_test_report():
+    curs = test_collection.find()
+    data_json = MongoJSONEncoder().encode(list(curs))
+    return json.loads(data_json)
 
 @hug.get('/happy_birthday')
 def happy_birthday(name, age:hug.types.number = 1):
-    id  = test_collection.insert_one({'name': name, 'age': age})
+    id  = test_collection.insert_one({'name': name, 'age': age, "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M")})
     print(id)
     return "happy {age} birthday {name}!".format(**locals())
